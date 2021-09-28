94608becd1d837636f95c74a02730b5618cb19fc
kazumasa.nakamura <kazumasa.nakamura@vitalize.co.jp>
2021-09-28 20:29:10 +0900

    "#2 create user entity"

---
+++ lib/Entity/user_entity.dart
@@ -0,0 +1,10 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user_entity.freezed.dart';
+part 'user_entity.g.dart';
+
+@freezed
+class UserEntity with _$UserEntity {
+  const factory UserEntity({required String name, required String email, required String password }) = _UserEntity;
+  factory UserEntity.fromJson(Map<String, dynamic> json) => _$UserEntityFromJson(json);
+} 
