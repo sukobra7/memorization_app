357ebe48a206801565a554d7f64b5bff0ceabdac
kazumasa.nakamura <kazumasa.nakamura@vitalize.co.jp>
2021-10-01 07:40:40 +0900

    "#3 create deck entity"

--- lib/Entity/deckEntity.dart
+++
@@ -1,9 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'deckEntity.freezed.dart';
-
-@freezed
-class DeckEntity with _$DeckEntity {
-  const factory DeckEntity({required String title, required String description, required int userId}) = _DeckEntity;
-    factory DeckEntity.fromJson(Map<String, dynamic> json) => _$DeckEntityFromJson(json);
-}
--- lib/Entity/userEntity.dart
+++
@@ -1,9 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'userEntity.freezed.dart';
-
-@freezed
-class UserEntity with _$UserEntity {
-  const factory UserEntity({required String name, required String email, required String password }) = _UserEntity;
-  factory UserEntity.fromJson(Map<String, dynamic> json) => _$UserEntityFromJson(json);
-} 
