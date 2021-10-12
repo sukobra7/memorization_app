8e335665ae888649e4f0017e29a25982b879e0e8
kazumasa.nakamura <kazumasa.nakamura@vitalize.co.jp>
2021-10-13 08:17:38 +0900

    "#19 create user auth entity"

---
+++ lib/Entity/user_auth_entity.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_auth_entity.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_UserAuthEntity _$_$_UserAuthEntityFromJson(Map<String, dynamic> json) {
+  return _$_UserAuthEntity(
+    email: json['email'] as String,
+    password: json['password'] as String,
+  );
+}
+
+Map<String, dynamic> _$_$_UserAuthEntityToJson(_$_UserAuthEntity instance) =>
+    <String, dynamic>{
+      'email': instance.email,
+      'password': instance.password,
+    };
